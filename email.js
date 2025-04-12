e028806d8a3949fdc9279cae2ec1ecd63d289a2d
Vishoo <vverma971@gmail.com>
2025-04-12 18:13:56 +0530

    form

---
+++ email.js
@@ -0,0 +1,16 @@
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent default form submission behavior
+
+      emailjs.sendForm('service_m1xxj92', 'template_qepq3uc', this)
+        .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('You will recieve a response from Florence Stone Concepts Team shortly!');
+        }, function(error) {
+          console.log('FAILED...', error);
+          alert('Failed to send your message. Please try again.');
+        });
+    });
+  };
+
--- script.js
+++ script.js
@@ -297,3 +297,42 @@ scrollToTopBtn.addEventListener("click", function () {
         behavior: "smooth" // Smooth scrolling effect
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+   
+   
+const formTrigger = document.querySelector('.form-trigger');
+const formPopup = document.querySelector('.form-popup');
+const closeBtn = document.querySelector('.close-btn');
+
+// Open popup
+formTrigger.addEventListener('click', () => {
+    formPopup.classList.add('active');
+});
+
+// Close popup
+closeBtn.addEventListener('click', () => {
+    formPopup.classList.remove('active');
+});
+});
+
+
+
+
+  window.onload = function() {
+
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent default form submission behavior
+
+      emailjs.sendForm('service_m1xxj92', 'template_qepq3uc', this)
+        .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('You will recieve a response from Florence Stone Concepts Team shortly!');
+        }, function(error) {
+          console.log('FAILED...', error);
+          alert('Failed to send your message. Please try again.');
+        });
+    });
+  };
+
